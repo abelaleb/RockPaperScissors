0d16c261f38abeb3511dcf5a3b3dd95dfdccf0dd
abel <abelalebachewasfaw.gmail.com>
2024-02-21 10:32:52 +0300

    Add a function that takes two parameters computerSelection and playerSelection and returns the victor or tie

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 //create a getcomputerchoice funciton to play as a computer 
-//create 
 let getComputerChoice = function(){
     let computerChoice = ["rock", "paper", "scissors"];
     no = Math.floor(Math.random()*3);
-    answer = computerChoice[no];
-    console.log(answer);
+    return computerChoice[no];
 }
+//create a fucntion that takes two parameters computerSelection and playerSelection.
+//And it returns a string that declares the winner or tie of the round like : "You Lose! Paper beats Rock"
+//Make you fucntion case insensitive
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection){
+        return "This round is a Tie."
+    }
+    else if( playerSelection == "rock" && computerSelection == "paper"){
+        return "You Lose! Paper beats Rock."}
+    else if (playerSelection == "rock" && computerSelection == "scissors"){
+        return "You Win! Rock beats Scissors."}
+  }
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playerSelection);
+  console.log(computerSelection);
+  let round = playRound(playerSelection, computerSelection)
+  console.log(round);
+  
